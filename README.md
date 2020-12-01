69266ae4cdb68c18c2770cc8c99a28fcfb160f92
kentaro-a <minminzemi1121@yahoo.co.jp>
2020-12-01 14:49:29 +0900

    F

--- README.md
+++ README.md
@@ -12,13 +12,13 @@ Set 32 bits mask gostatus.Bits as a status list and pass it to gostatus.NewStatu
 You can turn On/Off any bits using gostatus.Bits.  
 
 ```
-masks := map[string]Bits{
-"A": BIT_0,
-"B": BIT_1,
-"C": BIT_2,
+masks := map[string]gostatus.Bits{
+	"A": gostatus.BIT_0,
+	"B": gostatus.BIT_1,
+	"C": gostatus.BIT_2,
 }
 
-st := NewStatus(nil)
+st := gostatus.NewStatus(nil)
 fmt.Printf("[Initialized] bits: %s\n", st.GetBitsString())
 // [Initialized] bits: 00000000000000000000000000000000
 
@@ -38,4 +38,21 @@ st.Off(masks["B"])
 fmt.Printf("[Flag B are turned off] bits: %s\n", st.GetBitsString())
 // [Flag B are turned off] bits: 00000000000000000000000000000000
 
+st2 := gostatus.NewStatus(&gostatus.Option{gostatus.GetAllOnBits()})
+fmt.Printf("[Bits are all turned on] bits: %s\n", st2.GetBitsString())
+// [Bits are all turned on] bits: 11111111111111111111111111111111
+
+st2.SetBits(gostatus.GetAllOffBits())
+fmt.Printf("[Bits are all turned off] bits: %s\n", st2.GetBitsString())
+// [Bits are all turned off] bits: 00000000000000000000000000000000
+
+st2.SetBitsString("00001111000011110000111100001111")
+fmt.Printf("[Set Bits by string bit sequence] bits: %s\n", st2.GetBitsString())
+// [Bits are all turned off] bits: 00001111000011110000111100001111
+
+// Get Bits as uint32
+st2.Off(gostatus.GetAllOnBits())
+st2.On(gostatus.BIT_3 | gostatus.BIT_2 | gostatus.BIT_1 | gostatus.BIT_0)
+fmt.Printf("[As uint32] bits: %d\n", st2.GetBitsUint32())
+// [As uint32] bits: 15
 ```
--- examples/example.go
+++ examples/example.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-
 	masks := map[string]gostatus.Bits{
 		"A": gostatus.BIT_0,
 		"B": gostatus.BIT_1,
@@ -16,21 +15,39 @@ func main() {
 
 	st := gostatus.NewStatus(nil)
 	fmt.Printf("[Initialized] bits: %s\n", st.GetBitsString())
-	// [Initialized] bits: 00000000
+	// [Initialized] bits: 00000000000000000000000000000000
 
 	st.On(masks["A"])
 	fmt.Printf("[Flag A is turned on] bits: %s\n", st.GetBitsString())
-	// [Flag A is turned on] bits: 00000001
+	// [Flag A is turned on] bits: 00000000000000000000000000000001
 
 	st.On(masks["B"] | masks["C"])
 	fmt.Printf("[Flag B,C are turned on] bits: %s\n", st.GetBitsString())
-	// [Flag B,C are turned on] bits: 00000111
+	// [Flag B,C are turned on] bits: 00000000000000000000000000000111
 
 	st.Off(masks["A"] | masks["C"])
 	fmt.Printf("[Flag A,C are turned off] bits: %s\n", st.GetBitsString())
-	// [Flag A,C are turned off] bits: 00000010
+	// [Flag A,C are turned off] bits: 00000000000000000000000000000010
 
 	st.Off(masks["B"])
 	fmt.Printf("[Flag B are turned off] bits: %s\n", st.GetBitsString())
-	// [Flag B are turned off] bits: 00000000
+	// [Flag B are turned off] bits: 00000000000000000000000000000000
+
+	st2 := gostatus.NewStatus(&gostatus.Option{gostatus.GetAllOnBits()})
+	fmt.Printf("[Bits are all turned on] bits: %s\n", st2.GetBitsString())
+	// [Bits are all turned on] bits: 11111111111111111111111111111111
+
+	st2.SetBits(gostatus.GetAllOffBits())
+	fmt.Printf("[Bits are all turned off] bits: %s\n", st2.GetBitsString())
+	// [Bits are all turned off] bits: 00000000000000000000000000000000
+
+	st2.SetBitsString("00001111000011110000111100001111")
+	fmt.Printf("[Set Bits by string bit sequence] bits: %s\n", st2.GetBitsString())
+	// [Bits are all turned off] bits: 00001111000011110000111100001111
+
+	// Get Bits as uint32
+	st2.Off(gostatus.GetAllOnBits())
+	st2.On(gostatus.BIT_3 | gostatus.BIT_2 | gostatus.BIT_1 | gostatus.BIT_0)
+	fmt.Printf("[As uint32] bits: %d\n", st2.GetBitsUint32())
+	// [As uint32] bits: 15
 }
